79790ae99b318cc3238889d37b9be55f4d55c6c9
Jiri Jagos <jiri.jagos@gmail.com>
2012-05-22 00:29:14 +0200

    another exercises

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+age = raw_input('How old are you?')
+height = raw_input('How tall\'re you?')
+weight = raw_input('How much do you weigh?')
+
+print ("Age:\t%s\nHeight:\t%s\nWeight:\t%s\n") % (age, height, weight)
---
+++ ex13.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from sys import argv
+
+script, first, second, third = argv
+print ("ARGV is: %s %s %s %s") % (script, first, second, third)
